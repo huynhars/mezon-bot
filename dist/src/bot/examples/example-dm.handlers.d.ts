28ef478a0b9f36bc3cc5eff8f5ab09f620bf5a5b
huynhars <badhuynh4@gmail.com>
2026-01-11 21:22:30 +0700

    chore: remove build artifacts and update gitignore

--- dist/src/bot/examples/example-dm.handlers.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Nezon } from "@n0xgg04/nezon";
-export declare class ExampleDMHandlers {
-    onDMDemo(args: Nezon.Args, managedMessage: Nezon.AutoContextType.Message, dm: Nezon.AutoContextType.DM): Promise<void>;
-    onSendDMToSender(managedMessage: Nezon.AutoContextType.Message, user?: Nezon.User): Promise<void>;
-}
--- dist/src/bot/examples/example-mention.handlers.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Nezon } from "@n0xgg04/nezon";
-export declare class ExampleMentionHandlers {
-    mentionDemo([managedMessage]: Nezon.AutoContext, firstMention?: Nezon.Mention): Promise<void>;
-    multiMention([managedMessage]: Nezon.AutoContext, mentions: Nezon.Mentions): Promise<void>;
-    mentionRole(args: Nezon.Args, [managedMessage]: Nezon.AutoContext): Promise<void>;
-}
